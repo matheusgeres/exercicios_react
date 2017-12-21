9ba74b830b47a776daab82f5f4ac603c2f46222a
matheusgeres <matheusgeres@gmail.com>
2017-12-20 22:10:23 -0200

    Exercícios de React

---
+++ ex.5/component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => (
+    <h1>Primeiro Componente!</h1>
+)
+
+export const Segundo = props => <h1>Segundo Componente!</h1>
+
+// export{ Primeiro, Segundo }
---
+++ ex.7/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Family from './family'
+import Member from './member'
+
+ReactDOM.render(
+    <Family lastName='Silva'>
+        <Member name='Guilherme' />
+    </Family>
+, document.getElementById('app'))
